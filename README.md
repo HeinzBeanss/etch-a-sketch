4f1dd6fdae89007be8e3d30dc4b127e9a4c7251e
David Bean <heinzbeansss@gmail.com>
2021-10-20 07:42:21 +0100

    Finished to a satisfactory degree, functioning as expected although the code feels unecessary and messy.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # etch-a-sketch
-A simply drawing mechanic on a webpage
+A simply drawing mechanic on a webpage.
+
+
+-- After completion -- 
+It was fairly easy after I googled everything I needed to, it did bring up somethings I wasn't sure about but I'm sure I'll tackle those sooner rather than later. 
+Such things include why functions used in button events automatically activate as soon as the document loads if they have brackets at the end. Also I should practice how to incorporate values into functions depending on the value given, perhaps I need to use parameters in functions to do this and if so I need to look into them more. 
+
+Overall I have completed the task alongside some of the additional options. It's a minimal design so it's hard for it to look bad, but it doesn't exactly look good either, that being said visuals weren't exactly priority when creating this web page. I could have added a HTML colour picker as an option as well and I don't think it would have been too difficult but it's something I can explore at a later date. An issue I had was that the code was extremely messy, because of the things I don't necessarily understand I made my code longer than it needed to be.
+
+At the end of the day, it works and functions properly, however, I know that the code could be written more concisely and in a cleaner fashion, but function over form so it's something I'll work on as I generally get more accustomed to writing code. 
+
--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,32 +1,169 @@
-function createGrid() {
-    const grid = document.createElement("div");
-    grid.classList.add = ("grid");
-    body.prepend(grid);
+function createGridSmall() {
 
-    for (i=1; i <=large; i++) {
-        const line = document.createElement("div");
-        line.classList.add("line");
-        line.id="line" + i;
-        grid.appendChild(line);
+    mediumGrid.classList.remove("selected");
+    largeGrid.classList.remove("selected");
+    smallGrid.classList.add("selected");
 
+    while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+    }
+    
+    gridSize = 625;
+    squareSize = "bigSquare";
 
-
-    for (i=1; i <= large; i++) { //creating a line.
+    for (i=1; i <= gridSize; i++) { //creating a line.
         const square = document.createElement("div");
-        square.classList.add("square");
+        square.classList.add("square", squareSize);
         square.id = "sq" + i;
-        line.appendChild(square);
+        grid.appendChild(square);
+    }
+    blackSquare();
+}
+
+function createGridMedium() {
+
+    mediumGrid.classList.add("selected");
+    largeGrid.classList.remove("selected");
+    smallGrid.classList.remove("selected");
+
+    while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+    }
+    
+    gridSize = 2500;
+    squareSize = "mediumSquare";
+
+    for (i=1; i <= gridSize; i++) { //creating a line.
+        const square = document.createElement("div");
+        square.classList.add("square", squareSize);
+        square.id = "sq" + i;
+        grid.appendChild(square);
+    }
+    blackSquare();
+}
+
+function createGridLarge() {
+
+    mediumGrid.classList.remove("selected");
+    largeGrid.classList.add("selected");
+    smallGrid.classList.remove("selected");
+
+    while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+    }
+    
+    gridSize = 10000;
+    squareSize = "smallSquare";
+
+    for (i=1; i <= gridSize; i++) { //creating a line.
+        const square = document.createElement("div");
+        square.classList.add("square", squareSize);
+        square.id = "sq" + i;
+        grid.appendChild(square);
+    }
+    blackSquare();
+}
+
+function blackSquare() {
+    black.classList.add("selected");
+    rainbow.classList.remove("selected");
+    eraser.classList.remove("selected");
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+    square.addEventListener("mouseover", () => 
+    square.setAttribute("style", "background-color: black"))
+    });
+}
+
+function resetGrid() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+    square.removeAttribute("style", "background-color");
+    });
+    blackSquare();
+}
+
+function eraseSquare() {
+    black.classList.remove("selected");
+    rainbow.classList.remove("selected");
+    eraser.classList.add("selected");
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+    square.addEventListener("mouseover", () => 
+    square.removeAttribute("style", "background-color"));
+    });
+}
+
+function rainbowSquare() {
+    black.classList.remove("selected");
+    rainbow.classList.add("selected");
+    eraser.classList.remove("selected");
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    square.addEventListener("mouseover", () => 
+    square.setAttribute("style", `background-color:#${randomColor}`));
+    });
+}
+
+
+
+const container = document.querySelector(".container");
+const grid = document.querySelector(".grid");
+
+const reset = document.querySelector(".reset");
+reset.addEventListener("click", resetGrid);
+
+const black = document.querySelector(".black");
+black.addEventListener("click", blackSquare);
+
+const rainbow = document.querySelector(".rainbow");
+rainbow.addEventListener("click", rainbowSquare);
+
+const eraser = document.querySelector(".eraser");
+eraser.addEventListener("click", eraseSquare);
+
+const smallGrid = document.querySelector(".smallGrid");
+smallGrid.addEventListener("click", createGridSmall);
+
+const mediumGrid = document.querySelector(".mediumGrid");
+mediumGrid.addEventListener("click", createGridMedium);
+
+const largeGrid = document.querySelector(".largeGrid");
+largeGrid.addEventListener("click", createGridLarge);
+
+// const gridSize = 625; square size = 25px
+// const gridSize = 2500; square size = 12.5px
+// const gridSize = 10000; square size = 6.25px
+
+
+createGridSmall();
+blackSquare();
+
+/* function createGrid(gridSize) {
+
+    while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+    }
+
+    if (gridSize = "small") {
+        gridSize = 625;
+        squareSize = "bigSquare";
+    } else if (gridSize = "medium") {
+        gridSize = 2500;
+        squareSize = "mediumSquare";
+    } else if (gridSize = "large") {
+        gridSize = 10000;
+        squareSize = "smallSquare";
+    }
+
+    for (i=1; i <= gridSize; i++) { //creating a line.
+        const square = document.createElement("div");
+        square.classList.add("square", squareSize);
+        square.id = "sq" + i;
+        grid.appendChild(square);
+        
 }
 }
-}
-//each square will need an id between - 400 or even more in the future. 
+*/
 
-const body = document.querySelector("body");
-const large = 20; 
-
-
-
-
-
-
-createGrid();
